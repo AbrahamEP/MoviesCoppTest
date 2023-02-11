be551f597197f08110960e67eb194c6ae6eaf41a
Abraham Escamilla Pinelo <abraham_1992@hotmail.com>
2023-02-11 17:07:16 -0600

    Initial Commit

---
+++ MovieDB Test Coppel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB Test Coppel
+//
+//  Created by Abraham Escamilla Pinelo on 11/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
